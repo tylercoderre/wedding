c51b8f7c03638d7350897ae6e9657177343fe7b4
tylercoderre <39561384+tylercoderre@users.noreply.github.com>
2022-05-26 08:51:34 -0400

    Release 22.5.1
    Ported over 2 case studies from Coderre.Design (PCS & Cardus) and added 1 new case study (Paychex). Added a new contact form system. Started new accessibility tweaks with custom focus styling and other changes. Added content and general maintenance.

--- script.js
+++ script.js
@@ -27,7 +27,26 @@ checkbox.addEventListener('change', ()=> {
    
 });
 
+// DIALOG
 
+const openModal = document.querySelector('.open-modal');
+const closeModal = document.querySelector('.close-modal');
+const apply = document.querySelector('.apply');
+const dialog = document.querySelector('dialog');
+const input = document.querySelector('input');
+const output = document.querySelector('output');
+
+input.addEventListener('change', (e) => {
+    apply.value = e.target.value;
+});
+
+openModal.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+dialog.addEventListener('close', () => {
+    output.value = dialog.returnValue;
+});
 
 
 
